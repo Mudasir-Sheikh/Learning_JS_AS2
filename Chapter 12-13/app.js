4926abb42ae1f3670724ede75dd00e2192d05273
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-03-15 04:16:10 +0500

    Question 2 is added in chapter 12-13

---
+++ Chapter 12-13/app.js
@@ -0,0 +1,11 @@
+var num1 = +prompt("Enter the Integer 1:")
+var num2 = +prompt("Enter the Integer 2:")
+if (num1 > num2){
+    console.log(num1+" is greater than "+ num2)
+}
+else if(num1<num2){
+    console.log(num2 +" is greater than "+ num1)
+}
+else{
+    console.log("Both are Equal")
+}
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# This is My learning Javascript Journey Repository where i upload my javascript assignments.In this repo, i uploaded my javascript assignments of chap 12 to 13.
