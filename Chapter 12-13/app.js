bcfd5fc046c4c1ddc58b299c293d0a77373eff7c
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-03-15 18:27:36 +0500

    Except Question 1 All Questions are Done

--- Chapter 12-13/app.js
+++ Chapter 12-13/app.js
@@ -24,15 +24,64 @@
 // }
 
 
-var char = prompt("Enter only one letter or character")
-if (char.length === 1 && char.match(/[a-zA-Z]/)) {
-    if (char.toLowerCase() == "a" || char.toLowerCase() == "e" || char.toLowerCase() == "i" || char.toLowerCase() == "o" || char.toLowerCase() == "u") {
-        console.log("Character is Vowel")
-    }
-    else {
-        console.log("Character is Consonant")
-    }
+// var char = prompt("Enter only one letter or character")
+// if (char.length === 1 && char.match(/[a-zA-Z]/)) {
+//     if (char.toLowerCase() == "a" || char.toLowerCase() == "e" || char.toLowerCase() == "i" || char.toLowerCase() == "o" || char.toLowerCase() == "u") {
+//         console.log("Character is Vowel")
+//     }
+//     else {
+//         console.log("Character is Consonant")
+//     }
+// }
+// else {
+//     console.log("Input is Not a letter or Character")
+// }
+
+
+
+// //Question 05
+// // Account Login Password Checker
+// var inp_username = prompt("Enter Your Username")
+// var savedpassword = "javascript";
+// var inp_password;
+// inp_password = prompt("Enter your Password")
+// if (inp_password.length==0){
+//     console.log("Please enter your Password")
+// }
+// else if (inp_password == savedpassword){
+//     console.log("Your Password Matched the Original password \n Account Login Successfully")
+// }
+// else{
+//     console.log("Your Password is incorrect \n Try again")
+// }
+
+
+// //Question 06
+// //Corrected Version
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+
+//Question 07
+// Greetings with Time
+var time = +prompt("Enter The Time in 24-hour Format" , "Example: 1900")
+if (time > 2359 || time < 0){
+    console.log("Please Input Valid time")
 }
-else {
-    console.log("Input is Not a letter or Character")
+else if (time >= 0 && time < 1200){
+    console.log("Good Morning!")
+}
+else if (time>=1200 && time<=1700){
+    console.log("Good Afternoon!")
+}
+else if (time>=1700 && time<=2100){
+    console.log("Good Evening!")
+}else if (time>=2100 && time<=2359){
+    console.log("Good Night :)")
 }
