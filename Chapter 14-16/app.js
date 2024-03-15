ad97c0167cceb07bf55b641ace193ab50832a5e6
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-03-16 02:16:50 +0500

    Chapter 14-16 Question 01 -- 08 Done

---
+++ Chapter 14-16/app.js
@@ -0,0 +1,21 @@
+students_name =[]
+students_name_obj = new Array()
+string_Arr = ["computer","laptop","desktop","pc","mouse","keybord"]
+number_Arr = [1,2,3,4,5,6]
+Boolean_Arr = [true,false]
+mixed_Arr = ["workstation",100,true,"helloworld",null,200,false]
+
+qualification_array = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+console.log(qualification_array)
+
+
+studentName = []
+studentName.push("Ali","Saad","Saif"); //Using declared array
+studentsMarks=[280,320,410]
+totalmarks=500
+document.write("Score of Ali is ",studentsMarks[0]);
+document.write("Percentage: ",studentsMarks[0]/500*100)
+document.write("Score of Saad is ",studentsMarks[1]);
+document.write("Percentage: ",studentsMarks[1]/500*100)
+document.write("Score of Saif is ",studentsMarks[2]);
+document.write("Percentage: ",studentsMarks[2]/500*100)
