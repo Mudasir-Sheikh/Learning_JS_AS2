85728075d6b04212faf52cf124bff8a9ed5814a6
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-03-16 02:35:37 +0500

    Chapter 14-16 Question 10 -- 11 Done with var mistake in each question

--- Chapter 14-16/app.js
+++ Chapter 14-16/app.js
@@ -1,21 +1,49 @@
-students_name =[]
-students_name_obj = new Array()
-string_Arr = ["computer","laptop","desktop","pc","mouse","keybord"]
-number_Arr = [1,2,3,4,5,6]
-Boolean_Arr = [true,false]
-mixed_Arr = ["workstation",100,true,"helloworld",null,200,false]
+//Question 01
+var students_name =[]
 
-qualification_array = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+//Question 02
+var students_name_obj = new Array()
+
+//Question 03
+var string_Arr = ["computer","laptop","desktop","pc","mouse","keybord"]
+
+//Question 04
+var number_Arr = [1,2,3,4,5,6]
+
+//Question 05
+var Boolean_Arr = [true,false]
+
+//Question 06
+var mixed_Arr = ["workstation",100,true,"helloworld",null,200,false]
+
+//Question 07
+var qualification_array = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
 console.log(qualification_array)
 
-
-studentName = []
-studentName.push("Ali","Saad","Saif"); //Using declared array
-studentsMarks=[280,320,410]
-totalmarks=500
+//Question 08
+var studentName = []
+studentName.push("Ali","Saad","Saif"); //Using declared array above by push method.
+var studentsMarks=[281,320,430]
+var totalmarks=500
 document.write("Score of Ali is ",studentsMarks[0]);
-document.write("Percentage: ",studentsMarks[0]/500*100)
+document.write(" Percentage: ",studentsMarks[0]/500*100,"%<br>")
 document.write("Score of Saad is ",studentsMarks[1]);
-document.write("Percentage: ",studentsMarks[1]/500*100)
+document.write(" Percentage: ",studentsMarks[1]/500*100,"%<br>")
 document.write("Score of Saif is ",studentsMarks[2]);
-document.write("Percentage: ",studentsMarks[2]/500*100)
+document.write(" Percentage: ",studentsMarks[2]/500*100,"%<br><br>")
+
+
+
+//Question 09
+
+
+//Question 10
+var score_arr = [780,315,440,512,490,695,552,780] //Unsorted Array
+var score_arr_sorted = score_arr.sort()
+console.log(score_arr_sorted)
+
+
+var city_Names = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+var seleted_cities = city_Names.slice(2,4)
+console.log(city_Names)
+console.log(seleted_cities)
